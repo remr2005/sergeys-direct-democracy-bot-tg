51fb3c943010531e49c13061b1f801b79771da36
remr2005 <morpeh2005ka@gmail.com>
2024-06-19 13:49:34 +0300

    Made base

---
+++ config.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+api_id = os.getenv('ID')
+api_hash = os.getenv('HASH')
+bot_token = os.getenv('TOKEN')
--- main.py
+++ main.py
@@ -1,29 +1,27 @@
+from pyrogram import Client
+from config import api_id, api_hash, bot_token
 import os
 import importlib
-from pyrogram import Client, filters
+import logging
 
-# Ваши данные для подключения к Telegram API
-api_id = 'YOUR_API_ID'
-api_hash = 'YOUR_API_HASH'
-bot_token = 'YOUR_BOT_TOKEN'
+logger = logging.getLogger('pyrogram')
+logger.setLevel(logging.DEBUG)
 
-# Создание клиента
+# Инициализация клиента
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-# Загрузка команд из папки commands
-def load_commands(app):
-    commands_dir = 'commands'
-    for filename in os.listdir(commands_dir):
-        if filename.endswith('.py') and filename != '__init__.py':
-            module_name = f'{commands_dir}.{filename[:-3]}'
-            module = importlib.import_module(module_name)
-            for attribute_name in dir(module):
-                attribute = getattr(module, attribute_name)
-                if callable(attribute):
-                    command_name = filename[:-3]
-                    app.add_handler(filters.command(command_name)(attribute))
+# Автоматический импорт всех модулей из папки commands и регистрация команд
+commands_dir = 'commands'
+for filename in os.listdir(commands_dir):
+    if filename.endswith('.py') and filename != '__init__.py':
+        module_name = f"{commands_dir}.{filename[:-3]}"
+        module = importlib.import_module(module_name)
+        register_function = getattr(module, f"register_{filename[:-3]}_command")
+        register_function(app)
+        print(f"Imported and registered {module_name}")
 
-# Запуск бота и загрузка команд
-if __name__ == "__main__":
-    load_commands(app)
-    app.run()
+# Проверка запуска
+print("Bot is running...")
+
+# Запуск бота
+app.run()
