7f89572f727f33edf8c103f0da4368e5c3b8e037
vi-tr <144982881+vi-tr@users.noreply.github.com>
2024-06-21 08:33:37 +0000

    Added files via upload

--- commands/start.py
+++ commands/start.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 
 def register_start_command(app: Client):
-    @app.on_message(filters.command("start"))
+    @app.on_message(filters.command("start"))  
     async def start(client, message):
-        print("Start command received")
-        await message.reply("Hello! I'm your bot.")
+    	print("Start command received")  
+        await message.reply("Hello! I'm your bot.")  
