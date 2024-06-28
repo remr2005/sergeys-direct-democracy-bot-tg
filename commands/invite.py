d7f3d989db1b1b6e534500365471c19ae7fe7b33
remr2005 <morpeh2005ka@gmail.com>
2024-06-28 16:09:19 +0300

    сделал инвайт

--- commands/invite.py
+++ commands/invite.py
@@ -1,8 +1,31 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
+import json
+import vote
 
 def register_invite_command(app: Client):
     @app.on_message(filters.command("invite"))
-    async def invite(client, message):
+    async def invite(client: Client, message: Message):
         print("invite command received")
-        await message.reply('''В общем, здрасте. Список всех доступных команд:
-                            make_admin''')
+        # Берем параметры функции
+        args = message.text.split()[1:]
+        # Проверка на правильное написание TODO сделать более тонкую настройку админа
+        if len(args)>1:     
+            await message.reply("Ауч, вы написали какую то поеботу. Используйте /help invite, что бы узнать как пользоваться этой функцией")
+            return
+       
+        if await vote.vote(message,client,f"Дать ли ссылку инвайт {message.from_user.first_name}?",1):
+            # добавление
+            # try:
+            # Время, до которого пользователь будет забанен (например, на 1 день)
+            link_json = await client.create_chat_invite_link(message.chat.id)
+            link = dict(json.loads(str(link_json)))["invite_link"]
+            await message.reply(f"Юзеру {message.from_user.first_name} отправлено приглашение")
+            await client.send_message(
+                chat_id=message.from_user.id,
+                text=f"Привет, тебя приглашают в группу {link}")
+            # except:
+            #     await message.reply("Произошла ошибка")
+        else:
+            await message.reply("Голосование провалилось")
+
