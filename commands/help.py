32d50bf94ca6c374eccc944c5b438ac11a561ad8
remr2005 <morpeh2005ka@gmail.com>
2024-06-28 16:30:07 +0300

    немного сыра

--- commands/help.py
+++ commands/help.py
@@ -10,6 +10,7 @@ def register_help_command(app: Client):
             BotCommand("make_admin", "Дать пользователю админку в ходе голосования"),
             BotCommand("remove_admin", "Забрать у пользователя админку в ходе голосования"),
             BotCommand("kick", "Кикнуть пользователя в ходе голосования"),
+            BotCommand("invite", "Кинуть инвайт-ссылку тому кто начал голосование"),
         ]
         await client.set_bot_commands(commands)
         await message.reply("Команды успешно установлены!")
@@ -19,10 +20,10 @@ def register_help_command(app: Client):
         print("Help command received")
         args = message.text.split()[1:]
         if len(args)==0:
-            await message.reply('''В общем, здрасте. Список всех доступных команд:
+            await message.reply('''В общем, здрасте, наш ебейший сервак https://discord.gg/UEYQPsAuR4. Вы так же можете присоединиться к разработке бота, форкнув его с гитхаба: https://github.com/remr2005/sergeys-direct-democracy-bot-tg. Список всех доступных команд:
                                 make_admin
                                 remove_admin
-                                ~~invite~~
+                                invite
                                 kick
                                 ~~change_icon~~
                                 ~~change_name~~
@@ -39,4 +40,7 @@ def register_help_command(app: Client):
             await message.reply("""Команда /kick, создание голосование по поводу кика пользователя.
 Использование: /kick [user]
 Пример: /kick @example_user""")
+        elif args[0]=="invite":
+            await message.reply("""Команда /invite, создание голосование по киданию ссылки инвайта, тому кто начал голосование.
+Использование: /invite""")
             
