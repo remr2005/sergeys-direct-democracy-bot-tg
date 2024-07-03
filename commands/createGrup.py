333403d50f7819384da99a632697a7415d7725f8
remr2005 <morpeh2005ka@gmail.com>
2024-07-03 15:15:56 +0300

    add some fix

---
+++ commands/createGrup.py
@@ -0,0 +1,44 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import gradio_client
+from PIL import Image
+import os
+from random import random
+
+def register_createGrup_command(app: Client):
+    @app.on_message(filters.command("god_is_dead"))
+    async def createGrup(client: Client, message: Message):
+        print("create_group command received")
+        args = message.text.split()[1:]
+        text = ''.join([i+" " for i in args])
+        cl = gradio_client.Client("stabilityai/stable-diffusion-3-medium")
+        try:
+            word =""
+            a = random()
+            if a <=0.3:
+                word = "fox"
+            elif 0.3< a<=0.6:
+                word = "wolf"
+            elif 0.6<a<=0.9:
+                word = "tiger"
+            else:
+                word = "cat"
+            print(word)
+            result =  cl.predict(
+                    prompt=f"furry-{word} muscular 3d hentai "+text,
+                    negative_prompt="",
+                    randomize_seed=True,
+                    width=1024,
+                    height=1024,
+                    guidance_scale=5,
+                    num_inference_steps=28,
+                    api_name="/infer"
+            )
+        except Exception as e:
+            await message.reply(f"Произошла непредвиденная ошибка:{e}")
+        print(result)
+        image = Image.open(result[0])
+        image.save('output.png', 'png')
+        await client.send_photo(message.chat.id,"output.png")
+        os.remove("output.png")
+        os.remove(result[0])
